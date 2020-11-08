064a01dda5c703535a1255f3066e69483136ce71
sangeun <qufwkfl3024@gmail.com>
2020-11-08 12:16:56 +0900

    Update problem_1.py

--- Sangeun/src/week2/problem_1.py
+++ Sangeun/src/week2/problem_1.py
@@ -1,49 +1,23 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42626
-
-def heapify(array, cur_idx):
-    if cur_idx <= 0:
-        return array
-
-    left_child = cur_idx * 2
-    rigth_child = left_child + 1
-
-    smallest_idx = cur_idx
-
-    if (left_child < len(array)) and (array[left_child] < array[smallest_idx]):
-            smallest_idx = left_child
-    if (rigth_child < len(array)) and (array[rigth_child] < array[smallest_idx]):
-            smallest_idx = rigth_child
-
-    if smallest_idx != cur_idx:
-        array[cur_idx], array[smallest_idx] = array[smallest_idx], array[cur_idx]
-        array = heapify(array, smallest_idx)
-
-    array = heapify(array, cur_idx-1)
-    return array
-
+import heapq
 def solution(scoville, K):
     answer = 0
+    scov_heap = []
 
-    scoville_ = [0]
-    scoville_.extend(scoville)
+    for scov in scoville:
+        heapq.heappush(scov_heap, scov)
 
-    while len(scoville_) > 1:
-        min1, min2 = None, None
-        if len(scoville_) > 1:
-            scoville_ = heapify(scoville_, (len(scoville_) - 1) // 2)
-            min1 = scoville_[1]
-            scoville_.pop(1)
+    while scov_heap[0] < K:
+        if len(scov_heap) < 2 :
+            if scov_heap[0] < K:
+                return -1
+            break
 
-        if len(scoville_) > 1:
-            scoville_ = heapify(scoville_, (len(scoville_) - 1) // 2)
-            min2 = scoville_[1]
-            scoville_.pop(1)
+        min1 = scov_heap[0]
+        heapq.heappop(scov_heap)
+        min2 = scov_heap[0]
+        heapq.heappop(scov_heap)
+        heapq.heappush(scov_heap, (min1 + min2 * 2))
+        answer += 1
 
-        if (min1 is None) or (min2 is None):
-            return -1
-        else:
-            answer += 1
-            if (min1 + min2 * 2) >= K:
-                break
     return answer
-
