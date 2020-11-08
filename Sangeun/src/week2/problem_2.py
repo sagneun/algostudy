00fb02bfa4ceb5ddca0cbcd9da156da5da7c9c72
sangeun <qufwkfl3024@gmail.com>
2020-11-08 13:36:43 +0900

    Update problem_2.py

--- Sangeun/src/week2/problem_2.py
+++ Sangeun/src/week2/problem_2.py
@@ -1,18 +1,17 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42584
 from collections import deque
-
 def solution(prices):
     answer = []
-    dq = deque(prices)
-
-    sub_que = deque()
     for idx, price in enumerate(prices):
-
         cnt = 0
-        if idx < (len(prices) - 1):
-            sub_que.extend(prices[idx+1:])
+        if (idx+1) < len(prices):
+            sub_que = deque(prices[idx+1:])
             while len(sub_que) > 0:
-                cmp_price = sub_que.pop()
-                if cmp_price >= price:
+                cmp_price = sub_que.popleft()
+                if cmp_price < price:
+                    cnt += 1
+                    break
+                else:
                     cnt += 1
 
         answer.append(cnt)
